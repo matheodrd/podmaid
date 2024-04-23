a23389005cdb1979801ac8dbf4d3db9a8a0f7b99
matheodrd <matheodurand14@gmail.com>
2024-04-23 21:33:19 +0200

    add images list (ListView of Labels)

--- podmaid.py
+++ podmaid.py
@@ -1,7 +1,7 @@
 ﻿import podman
 from textual.app import App, ComposeResult
 from textual.binding import Binding
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, ListView, ListItem, Label
 
 def safe_get(ls: list, idx: int, default: any) -> str:
     try:
@@ -22,6 +22,13 @@ def get_images() -> dict:
             print("Impossible de se connecter à l'API de Podman\n\
                   Essayez : podman system service -t 0 &")
 
+def build_list_view() -> ListView:
+    list_view = ListView()
+    for _, img_data in get_images().items():
+        list_view.append(ListItem(Label(f"{img_data['short_id']} {img_data['tag']}")))
+        
+    return list_view
+
 class Podmaid(App):
     BINDINGS = [
         Binding(key="q", action="quit", description="Quit"),
@@ -32,6 +39,7 @@ class Podmaid(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield build_list_view()
 
     def on_mount(self) -> None:
         self.title = "Podmaid"
